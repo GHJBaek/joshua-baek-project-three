d8ff9b920e2bdd393cb8e852ddd98e29b395887b
GHJBaek <jbaek.dev@gmail.com>
2023-06-21 00:54:16 -0400

    added header sorting

--- src/components/SearchPlayerForm.js
+++ src/components/SearchPlayerForm.js
@@ -1,92 +1,87 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
-function SearchPlayerForm() {
+// // need to figure out how to create/fix the search endpoint
 
-    const [playerName, setPlayerName] = useState('');
-    const [player, setPlayer] = useState([]);
+// import axios from 'axios';
+// import { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
 
+// function SearchPlayerForm() {
 
-    useEffect(() => {
-
-        if (playerName) {
-            axios({
-                url: 'https://free-nba.p.rapidapi.com/players',
-                method: 'GET',
-                params: {
-                    page: '1',
-                    search: playerName
-                },
-                headers: {
-                    'X-RapidAPI-Key': 'edfe4dc115msh1dee64a40001ac3p1d2bcbjsnb8d4a9244952',
-                    'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
-                }
-            }).then((res) => {
-                // update state using the array returned to us from the API
-                const playerData = res.data.data;
-                setPlayer(playerData);
-                console.log(playerData);
-            })
-        }
-
-    }, [playerName]);
+//     const [playerName, setPlayerName] = useState('');
+//     const [player, setPlayer] = useState([]);
 
 
-    const handleInputChange = (e) => {
-        setPlayerName(e.target.value);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setPlayerName('');
+//     useEffect(() => {
 
-    };
+//         if (playerName) {
+//             axios({
+//                 url: 'https://ghjbaek.github.io/NBA-API/NBA-API.json',
+//                 method: 'GET',
+                
+//             }).then((res) => {
+//                 // update state using the array returned to us from the API
+//                 const playerData = res.data;
+//                 setPlayer(playerData);
+//             })
+//         }
 
-    return (
-        <div className="SearchPlayer">
+//     }, [playerName]);
 
-            <form action="" className='form'>
-                <label htmlFor="playerName">Search for a player </label>
-                <input
-                    id='playerName'
-                    value={playerName}
-                    onChange={handleInputChange}
-                    type="text" placeholder="Search for a player"
-                />
-                <button
-                    type="submit"
-                    onClick={handleSubmit}>Search</button>
-            </form>
 
-            <table className="player-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Player</th>
-                        <th>Position</th>
-                        <th>Team</th>
-                        <th>Conference</th>
+//     const handleInputChange = (e) => {
+//         setPlayerName(e.target.value);
+//     };
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         setPlayerName('');
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {player.map((player) => (
+//     };
 
-                        <tr key={player.id}>
-                            <td>{player.id}</td>
-                            <Link to={`/playerStat/${player.id}`}>
-                                <td>{player.first_name} {player.last_name}</td>
-                            </Link>
-                            <td>{player.position ?? '-'}</td>
-                            <td>{player.team.full_name ?? '-'}</td>
-                            <td>{player.team.conference ?? '-'}</td>
+//     return (
+//         <div className="SearchPlayer">
 
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-}
+//             <form action="" className='form'>
+//                 <label htmlFor="playerName">Search for a player </label>
+//                 <input
+//                     id='playerName'
+//                     value={playerName}
+//                     onChange={handleInputChange}
+//                     type="text" placeholder="Search for a player"
+//                 />
+//                 <button
+//                     type="submit"
+//                     onClick={handleSubmit}>Search</button>
+//             </form>
 
-export default SearchPlayerForm;
+//             <table className="player-table">
+//                 <thead>
+//                     <tr>
+//                         <th>ID</th>
+//                         <th>Player</th>
+//                         <th>Position</th>
+//                         <th>Team</th>
+//                         <th>Conference</th>
+
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     {player.map((player) => (
+
+//                         <tr key={player.id}>
+//                             <td>{player.id}</td>
+//                             <Link to={`/playerStat/${player.id}`}>
+//                                 <td>{player.first_name} {player.last_name}</td>
+//                             </Link>
+//                             <td>{player.position ?? '-'}</td>
+//                             <td>{player.team.full_name ?? '-'}</td>
+//                             <td>{player.team.conference ?? '-'}</td>
+
+//                         </tr>
+//                     ))}
+//                 </tbody>
+//             </table>
+//         </div>
+//     );
+// }
+
+// export default SearchPlayerForm;
